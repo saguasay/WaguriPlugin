062bbf8b73e1e682ae68aba166d19aadc9b6a7e4
0x_saguasay <maelmaeldu89@gmail.com>
2026-09-07 18:25:29 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { before } from "@vendetta/patcher";
+import { findByProps } from "@vendetta/metro";
+
+const WAGURI_GIFS = [
+  "https://media1.tenor.com/m/t-u0MbTWh7UAAAAC/kaoruko-waguri.gif",
+  "https://media1.tenor.com/m/ttJH4ujwboMAAAAC/kaoruko-waguri-kaoruko.gif",
+  "https://media1.tenor.com/m/qRT07oUZbDkAAAAC/waguri-kaoruko-kaoruhana.gif",
+  "https://media1.tenor.com/m/GyuGoM1OkEMAAAAC/kaoruko-waguri-kaoruko.gif",
+  "https://media1.tenor.com/m/3n3Vf_m0kPcAAAAC/kaoruko-waguri-waguri-kaoruko.gif",
+];
+
+function getRandomGif(): string {
+  return WAGURI_GIFS[Math.floor(Math.random() * WAGURI_GIFS.length)];
+}
+
+let unpatch: (() => void) | undefined;
+
+export default {
+  onLoad: () => {
+    const MessageActions = findByProps("sendMessage", "sendBotMessage");
+    unpatch = before("sendMessage", MessageActions, (args) => {
+      const message = args[1];
+      if (typeof message?.content === "string" && message.content.trim().toLowerCase() === "waguri") {
+        message.content = getRandomGif();
+      }
+    });
+  },
+  onUnload: () => {
+    unpatch?.();
+    unpatch = undefined;
+  },
+};
